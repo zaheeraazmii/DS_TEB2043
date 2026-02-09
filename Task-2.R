71c183eb763612c2c249baa14df3c29add93dd9d
zaheera <zaheeraazmii@gmail.com>
2026-02-09 10:32:10 +0800

    deleting files

--- Task-2.R
+++
@@ -1,9 +0,0 @@
-# Program to compare two strings (case-insensitive)
-
-string1 <- readline(prompt = "Enter string 1: ")
-string2 <- readline(prompt = "Enter string 2: ")
-
-are_same <- tolower(string1) == tolower(string2)
-
-cat("This program compare 2 strings.\n")
-cat("Both inputs are similar:", are_same, "\n")
